1569609c73d1b702ecc8cbba2313b9a9b122f1ca
Mark Patton <mark.patton@gmail.com>
2018-03-14 13:00:52 -0400

    Modify fedora wait script to use available credentials and only try a certain number of times.

--- .fedora-wait.sh
+++ .fedora-wait.sh
@@ -3,15 +3,30 @@
 # Read docker configuration
 . .env
 
+# Wait until we get a 200 from Fedora or fail some number of times.
+
 function wait_until_up {
-    CMD="curl -u bootstrap:bootstrap --write-out %{http_code} --silent -o /dev/stderr ${FEDORA_ADAPTER_BASE}"
+    CMD="curl -I -u admin:moo --write-out %{http_code} --silent -o /dev/stderr ${FEDORA_ADAPTER_BASE}"
     echo "Waiting for response from Fedora via ${CMD}"
-    RESULT=$(${CMD})
-    until [ ${RESULT} -lt 400 ] && [ ${RESULT} -gt 199 ]
+
+    RESULT=0
+    max=10
+    i=1
+    
+    until [ ${RESULT} -eq 200 ]
     do
-        echo "Trying again, result was ${RESULT}"
+        sleep 2
+        
         RESULT=$(${CMD})
-        sleep 1
+
+        if [ $i -eq $max ]
+        then
+           echo "Trying again, result was ${RESULT}"
+           exit 1
+        fi
+
+        i=$((i+1))
+        echo "Trying again, result was ${RESULT}"
     done
 
     echo "OK, fedora is up: ${RESULT}"
